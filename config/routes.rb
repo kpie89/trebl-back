96e29d22f278ca739d6a06821ad2dd962356c957
kpie89 <kpie89@gmail.com>
2016-04-03 19:00:54 -0400

    dummy route for soundcloud GET

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :comments, except: [:new, :edit]
   resources :playlists, except: [:new, :edit]
   resources :examples, except: [:new, :edit]
+  get '/tracks' => 'playlists#get_tracks'
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
