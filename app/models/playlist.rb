58ed7b2182893a721f9e0ac8a9a58043e138be65
kpie89 <kpie89@gmail.com>
2016-04-03 18:59:51 -0400

    initial GET from soundcloud

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,3 +1,12 @@
 class Playlist < ActiveRecord::Base
   has_many :comments
+
+  def self.get_tracks # get 10 hottest tracks
+    client = Soundcloud.new(client_id: 'b42fce0c05bc046484daed8a1de48bd8')
+    tracks = client.get('/tracks', :q => 'usher')
+    # print each link
+    tracks.each do |track|
+      puts track
+    end
+  end
 end
