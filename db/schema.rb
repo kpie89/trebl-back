25b1e37e51d0523c86debda1bddd0c8cb31916ea
kpie89 <kpie89@gmail.com>
2016-04-03 19:01:54 -0400

    add song column to playlists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403193705) do
+ActiveRecord::Schema.define(version: 20160403225513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160403193705) do
     t.string   "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "song"
   end
 
   create_table "profiles", force: :cascade do |t|
